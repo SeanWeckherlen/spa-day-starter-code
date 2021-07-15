c9c08aed5ea180ccd7581520e43571a0fc435871
Sean Weckherlen <sean_weckherlen@comcast.com>
2021-07-15 15:53:31 -0400

    completed chapter 12

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,30 @@
+package org.launchcode.spaday.controllers;
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/add")
+    public String displayAddUserForm() {
+        return "/user/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, @RequestParam String verifiedPassword) {
+        model.addAttribute("username", user.getUsername());
+
+        if (verifiedPassword.equals(user.getPassword())) {
+            return "/user/index";
+        } else {
+            model.addAttribute("email", user.getEmail());
+            model.addAttribute("error", "Passwords do not match, please re-enter your info");
+            return "/user/add";
+        }
+
+    }
+}
